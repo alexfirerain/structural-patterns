1a8e0395b11f7d3284b4f3e8fd1819e58bd62fe8
Alexander Lukyanov <lux-nova@mail.ru>
2021-09-21 13:09:43 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Задачи по структурным шаблонам
+***
+### [Calculator](.\calculator\src\main\java)
+
+Задача на *адаптер*: [lächerlich einfach und einfach lächerlich](. "(нем.) до смешного просто и просто смешно").
+
+### [Binary-Facade](.\binary-facade\src\main\java)
+
+Задача на *фасад*.
---
+++ binary-facade/src/main/java/BinOps.java
@@ -0,0 +1,15 @@
+public class BinOps {
+    public String sum(String a, String b) {
+    /*Переведите числа из двоичной записи в
+      строках в int, сложите и переведите
+      обратно в двоичную запись*/
+        return a + b;
+    }
+
+    public String mult(String a, String b) {
+    /*Переведите числа из двоичной записи в
+      строках в int, перемножьте и переведите
+      обратно в двоичную запись*/
+        return a + b;
+    }
+}
